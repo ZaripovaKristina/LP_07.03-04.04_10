296fdb750589afb2207aacac6f9b5f43abced6f8
Kristina <kkachulina@mail.ru>
2023-03-16 14:36:38 +0400

    Added Task20

---
+++ Task20/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 20: Напишите программу, которая
+// принимает на вход координаты двух точек и
+// находит расстояние между ними в 2D
+// пространстве.
+// A (3,6); B (2,1) -> 5,09
+// A (7,-5); B (1,-1) -> 7,21
+
+// double d = Math.Sqrt(5);
+// double res = 5.099987564;
+// double resRound = Math.Round(res, 2, MidpointRounding.ToZero);
+// Console.WriteLine(resRound);
+
+// Console.WriteLine("Введите координату Х первой точки: ");
+// double a1 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите координату Y перовой точки: ");
+// double a2 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите координату X второй точки: ");
+// double b1 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите координату Y второй точки: ");
+// double b2 = Convert.ToDouble(Console.ReadLine());
+// double resRound = Math.Round(Distance(a1, a2, b1, b2), 2, MidpointRounding.ToZero);
+// Console.WriteLine(resRound);
+
+// double Distance(double m, double n, double o, double p)
+// {
+//    return Math.Sqrt((m - o) * (m - o) + (n - p) * (n - p));
+// }
+
+Console.WriteLine("Введите координату X первой точки : ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y первой точки : ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату X второй точки : ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y второй точки : ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+double resRound = Math.Round(Distance(x1, y1, x2, y2), 2, MidpointRounding.ToZero);
+Console.WriteLine(resRound);
+
+double Distance(int aX, int aY,int bX,int bY)
+{
+   return Math.Sqrt((aX - bX) * ( aX - bX) + (aY - bY) * (aY - bY));
+}
+
+
+// double Distance(int x1, int y1, int x2, int y2)
+// {
+//     int x = x1 - x2;
+//     int y = y1 - y2;
+//     double c = Math.Sqrt(x * x + y * y);
+//     return c;
+// }
+
+// Console.WriteLine("Введите координату первой точки по x:");
+// int num1X = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату первой точки по y:");
+// int num1Y = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите координату второй точки по x:");
+// int num2X = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату второй точки по y:");
+// int num2Y = Convert.ToInt32(Console.ReadLine());
+
+// double result = Distance(num1X, num1Y, num2X, num2Y);
+// double resRound = Math.Round(result, 2, MidpointRounding.ToZero);
+
+// Console.WriteLine($"Расстояние между точками равно {resRound}");
