9fd0dc8eaf9888624370bb3953550b362c917ca4
Kristina <kkachulina@mail.ru>
2023-04-04 13:08:35 +0400

    Добавили Task41

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -21,8 +21,29 @@ void CheckingNumber(string number)
   else Console.WriteLine("Число НЕ является полиндромом");
 }
 
-if (number!.Length == 5)
-{
-  CheckingNumber(number);
-}
-else Console.WriteLine($"Число не ПЯТИзначное");
+// if (number!.Length == 5)
+// {
+//   CheckingNumber(number);
+// }
+// else Console.WriteLine($"Число не ПЯТИзначное");
+
+
+// bool Palindrome(int num)
+// {
+//     if(num < 10) return false;
+
+//     int initialNum = num;
+//     int reversNum = 0;
+
+//     while(num > 0)
+//     {
+//         reversNum = reversNum * 10 + num % 10;
+//         num /= 10;
+//     }
+//     return initialNum == reversNum;
+// }
+
+// Console.WriteLine("Введите число");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if(number < 0) number = number * -1;
