b05c62c6ef46b6986415c901b7fea889f58c79fd
Kristina <kkachulina@mail.ru>
2023-04-04 13:10:30 +0400

    Добавили Task52

---
+++ Task52/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] CreateMatrixRndInt(int rows, int columns)
+{                          // 0       1
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(0, 10);
+        }
+
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+void MatrixSum(int[,] array)
+{
+    Console.Write($"Среднее арифметическое каждого столбца: ");
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum += array[i, j];
+            if (i == array.GetLength(0) - 1) 
+            {
+                if (j == array.GetLength(0) - 1) Console.Write($"{Math.Round(sum/array.GetLength(0),1)}. "); 
+                else Console.Write($"{Math.Round(sum/array.GetLength(0),1)}; ");
+            }
+        }
+    }
+}
+
+int[,] array2d = CreateMatrixRndInt(4,4);
+PrintMatrix(array2d);
+MatrixSum(CreateMatrixRndInt(4,4));
