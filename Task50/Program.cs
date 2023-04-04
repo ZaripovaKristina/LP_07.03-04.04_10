b10b15f228a233a2e89c1dc356f265e085078e54
Kristina <kkachulina@mail.ru>
2023-04-04 13:28:31 +0400

    Добавили Task50

--- Task50/Program.cs
+++ Task50/Program.cs
@@ -13,52 +13,52 @@
 // 17 -> такого числа в массиве нет
 
 
-Console.WriteLine("введите номер строки");
-int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите номер столбца");
-int m = Convert.ToInt32(Console.ReadLine());
-int [,] myArray = CreateMatrixRndInt(3, 4, -10, 10);
-Console.WriteLine();
-PrintMatrix(myArray);
+// Console.WriteLine("введите номер строки");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите номер столбца");
+// int m = Convert.ToInt32(Console.ReadLine());
+// int [,] myArray = CreateMatrixRndInt(3, 4, -10, 10);
+// Console.WriteLine();
+// PrintMatrix(myArray);
 
-if (n > myArray.GetLength(0) || m > myArray.GetLength(1))
-{
-    Console.WriteLine("Такого элемента нет");
-}
-else
-{
-    Console.WriteLine($"Значение элемента строки {n}, столбца {m} -> {myArray[n-1,m-1]}");
-}
+// if (n > myArray.GetLength(0) || m > myArray.GetLength(1))
+// {
+//     Console.WriteLine("Такого элемента нет");
+// }
+// else
+// {
+//     Console.WriteLine($"Значение элемента строки {n}, столбца {m} -> {myArray[n-1,m-1]}");
+// }
 
 
-int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
-{                          // 0       1
-    int[,] matrix = new int[rows, columns];
-    Random rnd = new Random();
+// int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+// {                          // 0       1
+//     int[,] matrix = new int[rows, columns];
+//     Random rnd = new Random();
 
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            matrix[i, j] = rnd.Next(min, max + 1);
-        }
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = rnd.Next(min, max + 1);
+//         }
 
-    }
-    return matrix;
-}
+//     }
+//     return matrix;
+// }
 
-void PrintMatrix(int[,] matrix)
-{
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        Console.Write("|");
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            Console.Write($"{matrix[i, j], 4} ");
-        }
-        Console.WriteLine("|");
-    }
-}
+// void PrintMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         Console.Write("|");
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             Console.Write($"{matrix[i, j], 4} ");
+//         }
+//         Console.WriteLine("|");
+//     }
+// }
 
 
 
