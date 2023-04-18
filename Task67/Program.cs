8fa5df51f11b5ce06d6afd2c57082dbc8a2b1dc7
Kristina <kkachulina@mail.ru>
2023-04-18 14:27:32 +0400

    Добавили Task67

---
+++ Task67/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на 
+// вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+
+// Console.Clear();
+// Console.WriteLine("Введите целое положительное число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+int x = 321;;
+
+int SumDigits(int n)
+ {
+     if (n == 0) return 0;
+     else return n % 10 + SumDigits(n / 10);
+ }
+
+ Console.WriteLine(SumDigits(x)); // 3628800
